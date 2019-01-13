e3a5e438f3078d867ac9e8853c06a086980f81d2
C-Rodg <C-Rodg@users.noreply.github.com>
2019-01-12 21:03:52 -0800

    Added netlify form info

--- src/components/main.js
+++ src/components/main.js
@@ -112,7 +112,7 @@ class Main extends Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" data-netlify="true" name="contact">
             <div className="field half first">
               <label htmlFor="first-name">First Name</label>
               <input type="text" name="first-name" id="first-name" />
